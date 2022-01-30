6a03eff4663d61aeab04c9b40ec908309500d94e
root <labutov@satel.org>
2022-01-30 14:28:33 +0300

    CoinChangeProblem

---
+++ src/main/java/com/practice/hackerrank/recursion/CoinChangeProblem.java
@@ -0,0 +1,31 @@
+package com.practice.hackerrank.recursion;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CoinChangeProblem {
+    //    List<Integer> coins = Arrays.asList(1, 2, 5, 10, 12, 20, 50);
+    List<Integer> coins = Arrays.asList(1, 2, 5, 10, 12);
+
+    //    int sum = 65;
+    int sum = 15;
+    int count = 0;
+
+    int countMinCoins(int sum, int index) {
+        if (sum == 0)
+            return 1;
+        if (sumIsGreaterThanCoin(sum, coins.get(coins.size() - 1))) {
+            sum = sum - coins.get(coins.size() - index);//3
+            count++;
+        }
+        return count + countMinCoins(sum, index - 1);
+        for (int i = coins.size(); i > 0; i--) {
+            countMinCoins(sum, i);
+        }
+        return
+    }
+
+    boolean sumIsGreaterThanCoin(int sum, int nextCoin) {
+        return sum - nextCoin > 0 ? true : false;
+    }
+}
---
+++ src/test/java/hackerrank/recursion/CoinChangeProblemTest.java
@@ -0,0 +1,4 @@
+package hackerrank.recursion;
+
+public class CoinChangeProblemTest {
+}
