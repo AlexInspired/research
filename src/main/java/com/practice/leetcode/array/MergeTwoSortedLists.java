1b16a0bb1d4accf41c291109529bc057cbc92129
alex <labutov@satel.org>
2022-04-06 17:14:18 +0300

    MergeTwoSortedLists done

--- src/main/java/com/practice/leetcode/array/MergeTwoSortedLists.java
+++ src/main/java/com/practice/leetcode/array/MergeTwoSortedLists.java
@@ -2,7 +2,7 @@ package com.practice.leetcode.array;
 
 public class MergeTwoSortedLists {
 
-    //Definition for singly-linked list.
+
     public class ListNode {
         int val;
         ListNode next;
@@ -10,7 +10,7 @@ public class MergeTwoSortedLists {
         ListNode() {
         }
 
-        ListNode(int val) {
+        public ListNode(int val) {
             this.val = val;
         }
 
@@ -23,28 +23,31 @@ public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null && list2 == null) return null;
         ListNode result = new ListNode(0);
+
         ListNode currentNode = result;
         while (list1 != null && list2 != null) {
 
             if (list1.val <= list2.val) {
                 currentNode.next = list1;
+
                 list1 = list1.next;
             } else {
                 currentNode.next = list2;
-                list1 = list1.next;
+                list2 = list2.next;
             }
 
             currentNode = currentNode.next;
+
         }
         if (list2 != null) {
             currentNode.next = list2;
             list2 = list2.next;
-        }
-        else if(list1 != null){
+        } else if (list1 != null) {
             currentNode.next = list1;
             list1 = list1.next;
         }
-        return result;
+        return result.next;
     }
-
 }
+
+
