2642491c0e28ddf079fedff8aff1896ea8f239a2
alex <labutov@satel.org>
2022-04-06 16:59:56 +0300

    MergeTwoSortedLists

--- src/main/java/com/practice/leetcode/array/MergeTwoSortedLists.java
+++ src/main/java/com/practice/leetcode/array/MergeTwoSortedLists.java
@@ -22,36 +22,29 @@ public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null && list2 == null) return null;
-        ListNode result = null;
-        if (list1.val <= list2.val) {
-            result = list1;
-            doHandle(list1, list2);
-        } else {
-            result = list2;
-            doHandle(list2, list1);
+        ListNode result = new ListNode(0);
+        ListNode currentNode = result;
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                currentNode.next = list1;
+                list1 = list1.next;
+            } else {
+                currentNode.next = list2;
+                list1 = list1.next;
+            }
+
+            currentNode = currentNode.next;
+        }
+        if (list2 != null) {
+            currentNode.next = list2;
+            list2 = list2.next;
+        }
+        else if(list1 != null){
+            currentNode.next = list1;
+            list1 = list1.next;
         }
         return result;
     }
-
-    private ListNode doHandle(ListNode list1, ListNode list2) {
-
-        ListNode current1 = list1.next;
-        ListNode current2 = list2;
-
-        while (true) {
-            if (current1 == null && current2 == null) return list1;
-            if (current1 != null) {
-                if (current1.val <= current2.val ) {
-                    list1 = current1;
-                    current1 = current1.next;
-                } else {
-                    list1.next = current2;
-                    current2 = current2.next;
-                }
-            }
-            //handle situation when one of current pointers becomes null
-        }
-    }
-
 
 }
--- src/test/java/leetcode/array/MergeTwoSortedListsTest.java
+++ src/test/java/leetcode/array/MergeTwoSortedListsTest.java
@@ -1,23 +1,21 @@
 package leetcode.array;
 
 import com.practice.leetcode.array.MergeTwoSortedLists;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class MergeTwoSortedListsTest {
 
     @Test
-    public void testTwoSum() {
+    public void testMergeTwoSortedListsTest() {
         MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
 
-        MergeTwoSortedLists.ListNode n13 = mergeTwoSortedLists.new ListNode(10, null);
-        MergeTwoSortedLists.ListNode n12 = mergeTwoSortedLists.new ListNode(8, n13);
+        MergeTwoSortedLists.ListNode n13 = mergeTwoSortedLists.new ListNode(4, null);
+        MergeTwoSortedLists.ListNode n12 = mergeTwoSortedLists.new ListNode(2, n13);
         MergeTwoSortedLists.ListNode n11 = mergeTwoSortedLists.new ListNode(1, n12);
 
-        MergeTwoSortedLists.ListNode n24 = mergeTwoSortedLists.new ListNode(9, null);
-        MergeTwoSortedLists.ListNode n23 = mergeTwoSortedLists.new ListNode(8, n24);
-        MergeTwoSortedLists.ListNode n22 = mergeTwoSortedLists.new ListNode(6, n23);
-        MergeTwoSortedLists.ListNode n21 = mergeTwoSortedLists.new ListNode(5, n22);
+        MergeTwoSortedLists.ListNode n23 = mergeTwoSortedLists.new ListNode(4, null);
+        MergeTwoSortedLists.ListNode n22 = mergeTwoSortedLists.new ListNode(3, n23);
+        MergeTwoSortedLists.ListNode n21 = mergeTwoSortedLists.new ListNode(1, n22);
 
         mergeTwoSortedLists.mergeTwoLists(n11, n21);
     }
