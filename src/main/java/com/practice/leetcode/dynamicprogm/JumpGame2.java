9057246de5c87f3692500a4b07ba82727fa3517d
labutov.av <avlabutov@gmail.com>
2022-06-11 15:45:18 +0300

    45. Jump Game II recursive version(leetcode TLE)

--- src/main/java/com/practice/leetcode/dynamicprogm/JumpGame2.java
+++ src/main/java/com/practice/leetcode/dynamicprogm/JumpGame2.java
@@ -2,19 +2,21 @@ package com.practice.leetcode.dynamicprogm;
 
 public class JumpGame2 {
 
-    int[] given = {2,3,1,1,4};
     int minJumps = Integer.MAX_VALUE;
 
     public int jump(int[] nums) {
+        if(nums.length == 1) return 0;
         return minJumps(nums, 0, 0);
     }
 
     private int minJumps(int[] nums, int currentPosition, int jumpsCounter) {
-        if(currentPosition >= nums.length){
+        if (currentPosition >= nums.length - 1) {
             minJumps = Math.min(minJumps, jumpsCounter);
             return minJumps;
         }
-        for(int i = currentPosition; i < nums[currentPosition];){
+        if (nums[currentPosition] == 0) return minJumps;
+
+        for (int i = currentPosition; i < (currentPosition + nums[currentPosition]); jumpsCounter--) {
             minJumps(nums, ++i, ++jumpsCounter);
         }
         return minJumps;
--- src/test/java/com/practice/leetcode/dynamicprogm/JumpGame2Test.java
+++ src/test/java/com/practice/leetcode/dynamicprogm/JumpGame2Test.java
@@ -1,22 +1,18 @@
 package com.practice.leetcode.dynamicprogm;
 
-import com.practice.leetcode.array.RotateImage;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class JumpGame2Test {
-
-
     @Test
-    public void test(){
+    public void test() {
         JumpGame2 jumpGame2 = new JumpGame2();
-        int[] given = {2,3,1,1,4};
+//        int[] given = {2,3,1,1,4};
+        int[] given = {2,1};
 
         int result = jumpGame2.jump(given);
 
-        Assert.assertEquals(3, result);
+        Assert.assertEquals(1, result);
     }
 
 }
