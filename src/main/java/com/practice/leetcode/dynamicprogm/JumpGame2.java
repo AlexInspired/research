079cb1a89cda8203c8536f4b155733c424517fb7
labutov.av <avlabutov@gmail.com>
2022-06-22 17:38:26 +0300

    45. Jump Game II greedy version

--- src/main/java/com/practice/leetcode/dynamicprogm/JumpGame2.java
+++ src/main/java/com/practice/leetcode/dynamicprogm/JumpGame2.java
@@ -2,23 +2,36 @@ package com.practice.leetcode.dynamicprogm;
 
 public class JumpGame2 {
 
-    int minJumps = Integer.MAX_VALUE;
-
     public int jump(int[] nums) {
-        if(nums.length == 1) return 0;
+        if (nums.length == 1) return 0;
         return minJumps(nums, 0, 0);
     }
 
     private int minJumps(int[] nums, int currentPosition, int jumpsCounter) {
-        if (currentPosition >= nums.length - 1) {
-            minJumps = Math.min(minJumps, jumpsCounter);
-            return minJumps;
-        }
-        if (nums[currentPosition] == 0) return minJumps;
 
-        for (int i = currentPosition; i < (currentPosition + nums[currentPosition]); jumpsCounter--) {
-            minJumps(nums, ++i, ++jumpsCounter);
-        }
-        return minJumps;
+        if (nums[currentPosition] == 0) return jumpsCounter;
+        int nextPosition = determineNextPosition(nums, currentPosition);
+        if (nextPosition == -1) return ++jumpsCounter;
+        jumpsCounter = minJumps(nums, nextPosition, ++jumpsCounter);
+
+        return jumpsCounter;
     }
+
+    private int determineNextPosition(int[] nums, int currentPosition) {
+        int maxJumpRange = (currentPosition + nums[currentPosition]);
+
+        int maxJumpInx = maxJumpRange;
+        if (maxJumpRange >= nums.length - 1) {
+            return -1;
+        }
+        for (int i = currentPosition + 1; i <= currentPosition + nums[currentPosition]; i++) {
+            if (nums[i] + i >= maxJumpRange) {
+                maxJumpInx = i;
+                maxJumpRange = nums[i] + i;
+            }
+            if(maxJumpRange >= nums.length) break;
+        }
+        return maxJumpInx;
+    }
+
 }
--- src/test/java/com/practice/leetcode/dynamicprogm/JumpGame2Test.java
+++ src/test/java/com/practice/leetcode/dynamicprogm/JumpGame2Test.java
@@ -7,12 +7,11 @@ public class JumpGame2Test {
     @Test
     public void test() {
         JumpGame2 jumpGame2 = new JumpGame2();
-//        int[] given = {2,3,1,1,4};
-        int[] given = {2,1};
-
+//        int[] given = {10,9,8,7,6,5,4,3,2,1,1,0};
+        int[] given = {4,1,1,3,1,1,1};
         int result = jumpGame2.jump(given);
 
-        Assert.assertEquals(1, result);
+        Assert.assertEquals(2, result);
     }
 
 }
